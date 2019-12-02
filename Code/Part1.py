826b3c55ae36d1c0ed7b14cd9cd7120a6bdd1552
PMulholland95 <56317702+PMulholland95@users.noreply.github.com>
2019-12-02 22:25:36 +0100

    Update Part1.py
    I've added the code from line 37 here.
    This completes the simulation up to creating a csv file (for our completed adj_matrix dataframe).

--- Code/Part1.py
+++ Code/Part1.py
@@ -6,6 +6,7 @@
 
 import pandas as pd
 import numpy as np
+import time
 
 
 file_path = "/Users/sergiopicella/Downloads/ca-GrQc.txt"
@@ -29,5 +30,51 @@ for j in range(0,n):
 df_a_sorted = df_a.sort_values(by=['from'])
 
 adj_a = pd.DataFrame(np.zeros(shape=[n,n]),dtype='bool')
-#adj_a
 
+#adj_a is now an nxn dataframe matrix, with all entries equal to "False"
+
+
+#Paul added the following:
+
+#The following code now changes appropriate elements of adj_a to "True"
+#These "True" elements indicate the presence of an edge in our network
+#This process has a timer also
+
+
+start = time.process_time() 
+for i in range(0,len(df_a_sorted)):
+    adj_a[df_a_sorted.iloc[i][0]][df_a_sorted.iloc[i][1]]=True
+    
+    if i == int(len(df_a_sorted)*.2):
+        lap = time.process_time()
+        print("20% of adj matrix filled")
+        print("Elapsed time = %.2f seconds\n" % (lap-start))
+        
+    if i == int(len(df_a_sorted)*.4):
+        lap = time.process_time()
+        print("40% of adj matrix filled")
+        print("Elapsed time = %.2f seconds\n" % (lap-start))
+        
+    if i == int(len(df_a_sorted)*.6):
+        lap = time.process_time()
+        print("60% of adj matrix filled")
+        print("Elapsed time = %.2f seconds\n" % (lap-start))
+        
+    if i == int(len(df_a_sorted)*.8):
+        lap = time.process_time()
+        print("80% of adj matrix filled")
+        print("Elapsed time = %.2f seconds\n" % (lap-start))
+        
+    if i == int(len(df_a_sorted)-1):
+        lap = time.process_time()
+        print("100% of adj matrix filled")
+        print("Elapsed time = %.2f seconds\n" % (lap-start))
+        
+        
+end = time.process_time()-start 
+print("It took me %.2f seconds "%(end))
+
+
+#This saves our adj_a dataframe to a csv file on user's computer
+
+adj_a.to_csv("Adj_a.csv", header = False, index = False)
